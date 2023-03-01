1617914fe650e29a6dec7b0ad83ecdaf3a513bcb
David Dume <dumedavid11@gmail.com>
2023-03-01 16:39:42 +0100

    aggiunta di input e di stili

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const spesa = ["mele", "pere", "carote", "pane", "patate"];
-const container = document.querySelector(".container");
+const spesa = [];
+
+const lista = document.querySelector(".lista");
+const input = document.querySelector("#item");
+
+const btn = document.querySelector('#btn')
+
 
 let contatore = 0;
 
-while (contatore < spesa.length - 1) {
-    container.innerHTML += `<h3>${spesa[contatore]}</h3>`
-    contatore++;
-}
+btn.addEventListener('click', function() {
+    if(input.value != "") {
+        spesa.push(input.value);
+        input.value = "";
+        while (contatore <= spesa.length - 1) {
+            lista.innerHTML += `<h3>${spesa[contatore]}</h3>`
+            contatore++;
+        }
+    }
+    
+});
+
